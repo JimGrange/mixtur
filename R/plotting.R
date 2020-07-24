d3ba046020e92dd0795abc464d03ac75a0259903
Jim Grange <grange.jim@gmail.com>
2020-07-24 09:29:48 +0100

    started error_plot

--- R/plotting.R
+++ R/plotting.R
@@ -12,7 +12,7 @@
 #' number ('id_var'); the participants' response per trial ('response_var');
 #' the  target value ('target_var'); and, if applicable, the condition
 #' ('condition_var').
-#'@param unit The unit of measurement in the data frame: "degrees"
+#'@param unit The unit of measurement in the data frame: "degrees_360"
 #'(measurement is in degrees, from 0 to 360); "degrees_180 (measurement is in
 #'degrees, but limited to 0 to 180); "radians" (measurement is in radians,
 #'from pi to 2 * pi); "wrapped_radians" (measurement is in radians, but
@@ -26,7 +26,7 @@
 #'that codes for the current condition
 #' @export
 plot_error <- function(data,
-                       unit = "degrees",
+                       unit = "degrees_360",
                        id_var = "id",
                        response_var = "response",
                        target_var = "target",
@@ -34,7 +34,64 @@ plot_error <- function(data,
                        condition_var = "condition"){
 
 
+  # establish the break points of the density plot
+  break_points <- round(seq(from = -pi, to = pi, length.out = 18), 3)
+
+  # get the list of participant ids
+  ids <- unique(data[[id_var]])
 
 
+  # calculate response error mapped onto circular space ----
+  if(unit == "degrees_360"){
+    response <- data[[response_var]] / 180 * pi
+    target <- data[[target_var]] / 180 * pi
+    data$error <- wrap(response - target)
+  }
+
+  if(unit == "degrees_180"){
+    response <- data[[response_var]] / 90 * pi
+    target <- data[[target_var]] / 90 * pi
+    data$error <- wrap(response - target)
+  }
+
+  if(unit == "radians"){
+    response <- data[[response_var]]
+    target <- data[[target_var]]
+    data$error <- response - target
+  }
+
+  if(unit == "wrapped_radians"){
+    data$error <- data$response
+  }
+
+
+
+# find mean error ---------------------------------------------------------
+  if(is.null(set_size) && is.null(condition_var)){
+
+    final_data <- data %>%
+      group_by(id) %>%
+      summarise(y = hist(error, breaks = break_points, plot = FALSE)$density,
+                x = hist(error, breaks = break_points, plot = FALSE)$mids) %>%
+      group_by(x) %>%
+      summarise(mean_error = mean(y),
+                se_error = (sd(y) / sqrt(length(y))))
+  }
+
+
+
+# plot the data -----------------------------------------------------------
+
+ggplot(final_data, aes(x = x,
+                       y = mean_error)) +
+    geom_point() +
+    geom_errorbar(aes(ymax = mean_error + se_error,
+                      ymin = mean_error - se_error),
+                  width = 0.05) +
+    theme_bw() +
+    scale_x_continuous(limits = c(-pi, pi))
+
+
+return(data)
 
 }
--- R/utilities.R
+++ R/utilities.R
@@ -1,1 +1,34 @@
 ### various utility functions
+
+# degrees to radians ------------------------------------------------------
+#' Transform degrees into radians
+#'
+#' A function to transform degrees into radians
+#' @param deg Degree value to transform into radians
+#' #' @export
+degrees_to_radians <- function(deg){deg * pi / 180}
+
+
+
+# radians to degrees ------------------------------------------------------
+#' Transform radians into degrees
+#'
+#' A function to transform radians into degrees
+#' @param deg Radian value to transform into degrees
+#' #' @export
+radians_to_degrees <- function(rad) {(rad * 180) / (pi)}
+
+
+
+# wrapping radians onto circular space ------------------------------------------
+#' Map values onto circular space
+#' This function maps the input radians onto the circular space -pi to pi.
+#' @param data The data (in radians) to be mapped
+#' #' @export
+wrap <- function(data, bound = pi) {
+  result <- ((data + bound) %% (bound * 2)) - bound
+  return(result)
+}
+
+
+
