9a61666be1b434d8d26d3d1859068a6f1a1fbead
Jim Grange <grange.jim@gmail.com>
2020-10-16 09:21:10 +0100

    corrected berry data date

--- data-raw/berry_2019.R
+++ data-raw/berry_2019.R
@@ -1,10 +1,10 @@
 # downloaded from https://osf.io/8wzgq/
 library(tidyverse)
-berry_2009 <- read.csv("berry_2009.csv")
+berry_2019 <- read.csv("berry_2019.csv")
 
-berry_2009 <- berry_2009 %>%
+berry_2019 <- berry_2019 %>%
   select(id, condition, target_ori, response_ori, non_target_1, non_target_2)
 
-save(berry_2009, file = "berry_2009.rda")
+save(berry_2019, file = "berry_2019.rda")
 
 
