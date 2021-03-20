6f7363efcf96b229aad92dec11ccbeb1d26cd94e
Jim Grange <grange.jim@gmail.com>
2021-03-20 14:11:00 +0000

    added mean absolute error and resultant vector length plus a function to obtain all summary stats

--- R/analysing.R
+++ R/analysing.R
@@ -1,3 +1,188 @@
+
+# get summary statistics --------------------------------------------------
+
+#' Obtain summary statistics of response error
+#'
+#' Returns participant-level summary statistic data of response error estimates
+#' ready for inferential analysis. Note that the function does not actually
+#' conduct the analysis.
+#'
+#' @param data A data frame with columns containing: participant identifier
+#' (declared via variable 'id_var'); the participants' response per trial
+#' ('response_var'); the target value ('target_var'); and, if applicable, the
+#' set size of each response ('set_size_var'), and the condition of each
+#' response ('condition_var').
+#' @param unit The unit of measurement in the data frame: "degrees"
+#' (measurement is in degrees, from 0 to 360); "degrees_180 (measurement is in
+#' degrees, but limited to 0 to 180); or "radians" (measurement is in radians,
+#' from pi to 2 * pi, but could also be already in -pi to pi).
+#' @param id_var The quoted column name coding for participant id. If the data
+#' is from a single participant (i.e., there is no id column) set to NULL.
+#' @param response_var The quoted column name coding for the participants'
+#' responses
+#' @param target_var The quoted column name coding for the target value.
+#' @param set_size_var The quoted column name (if applicable) coding for the
+#' set size of each response.
+#' @param condition_var The quoted column name (if applicable) coding for the
+#' condition of each response.
+#' @return \code{mean_abs_error} The mean absolute error of the participant
+#' response.
+#' @return \code{resultant_vector_length} The (normalised) resultant vector
+#' length of the response errors.
+#' @return \code{precision} The reciprocal of the circular standard deviation of
+#' the response error, with correction for guessing.
+#' @return \code{bias} The bias of the precision estimate.
+#' @importFrom stats sd
+#' @importFrom dplyr %>%
+#' @importFrom dplyr summarise
+#' @importFrom dplyr group_by
+#' @importFrom graphics hist
+#'
+#'
+#' @examples
+#' # load an example data frame
+#' data(bays2009_full)
+#'
+#' # calculate the summary statistics per condition per set size
+#'summary_data <- summary_statistics(data = bays2009_full,
+#'                                   unit = "radians",
+#'                                   condition_var = "delay",
+#'                                   set_size_var = "set_size")
+#'
+#' @export
+summary_statistics <- function(data,
+                               unit = "degrees",
+                               id_var = "id",
+                               response_var = "response",
+                               target_var = "target",
+                               set_size_var = NULL,
+                               condition_var = NULL){
+
+
+  # get the list of participant ids
+  ids <- unique(data[[id_var]])
+
+  # calculate response error mapped onto circular space ----
+  if(unit == "degrees"){
+    response <- data[[response_var]] / 180 * pi
+    target <- data[[target_var]] / 180 * pi
+    data$error <- wrap(response - target)
+  }
+
+  if(unit == "degrees_180"){
+    response <- data[[response_var]] / 90 * pi
+    target <- data[[target_var]] / 90 * pi
+    data$error <- wrap(response - target)
+  }
+
+  if(unit == "radians"){
+    response <- data[[response_var]]
+    target <- data[[target_var]]
+    data$error <- wrap(response - target)
+  }
+
+  if(unit == "wrapped_radians"){
+    data$error <- data$response
+  }
+
+
+
+  # no set size or condition manipulation
+  if(is.null(set_size_var) && is.null(condition_var)){
+
+    if(!is.null(id_var)){
+      final_data <- data %>%
+        group_by(id) %>%
+        summarise(mean_abs_error = get_mean_absolute_error(error),
+                  resultant_vector_length = get_resultant_vector_length(error),
+                  precision = get_precision_single(error)[, 1],
+                  bias = get_precision_single(error)[, 2])
+    } else{
+      final_data <- data %>%
+        summarise(mean_abs_error = get_mean_absolute_error(error),
+                  resultant_vector_length = get_resultant_vector_length(error),
+                  precision = get_precision_single(error)[, 1],
+                  bias = get_precision_single(error)[, 2])
+    }
+
+  }
+
+  # no set size manipulation but there is a condition manipulation
+  if(is.null(set_size_var) && !is.null(condition_var)){
+
+    data$condition <- as.factor(data[[condition_var]])
+
+    if(!is.null(id_var)){
+      final_data <- data %>%
+        group_by(id, condition) %>%
+        summarise(mean_abs_error = get_mean_absolute_error(error),
+                  resultant_vector_length = get_resultant_vector_length(error),
+                  precision = get_precision_single(error)[, 1],
+                  bias = get_precision_single(error)[, 2])
+    } else{
+      final_data <- data %>%
+        group_by(condition) %>%
+        summarise(mean_abs_error = get_mean_absolute_error(error),
+                  resultant_vector_length = get_resultant_vector_length(error),
+                  precision = get_precision_single(error)[, 1],
+                  bias = get_precision_single(error)[, 2])
+    }
+
+  }
+
+  # set size manipulation, but no condition manipulation
+  if(!is.null(set_size_var) && is.null(condition_var)){
+
+    data$set_size <- data[[set_size_var]]
+
+    if(!is.null(id_var)){
+      final_data <- data %>%
+        group_by(id, set_size) %>%
+        summarise(mean_abs_error = get_mean_absolute_error(error),
+                  resultant_vector_length = get_resultant_vector_length(error),
+                  precision = get_precision_single(error)[, 1],
+                  bias = get_precision_single(error)[, 2])
+    } else{
+      final_data <- data %>%
+        group_by(set_size) %>%
+        summarise(mean_abs_error = get_mean_absolute_error(error),
+                  resultant_vector_length = get_resultant_vector_length(error),
+                  precision = get_precision_single(error)[, 1],
+                  bias = get_precision_single(error)[, 2])
+    }
+
+  }
+
+  # both set size & condition manipulation
+  if(!is.null(set_size_var) && !is.null(condition_var)){
+
+    data$set_size <- data[[set_size_var]]
+    data$condition <- as.factor(data[[condition_var]])
+
+    if(!is.null(id_var)){
+      final_data <- data %>%
+        group_by(id, condition, set_size) %>%
+        summarise(mean_abs_error = get_mean_absolute_error(error),
+                  resultant_vector_length = get_resultant_vector_length(error),
+                  precision = get_precision_single(error)[, 1],
+                  bias = get_precision_single(error)[, 2])
+    } else{
+      final_data <- data %>%
+        group_by(condition, set_size) %>%
+        summarise(mean_abs_error = get_mean_absolute_error(error),
+                  resultant_vector_length = get_resultant_vector_length(error),
+                  precision = get_precision_single(error)[, 1],
+                  bias = get_precision_single(error)[, 2])
+
+    }
+  }
+
+
+return(final_data)
+
+}
+
+
 # analyse behavioural precision ----------------------------------------------
 #' Obtain measures of precision
 #'
@@ -26,6 +211,9 @@
 #' size of each response.
 #' @param condition_var The quoted column name (if applicable) coding for the
 #' condition of each response.
+#' @return \code{precision} The reciprocal of the circular standard deviation of
+#' the response error, with correction for guessing.
+#' @return \code{bias} The bias of the precision estimate.
 #' @importFrom stats sd
 #' @importFrom dplyr %>%
 #' @importFrom dplyr summarise
@@ -191,5 +379,42 @@ get_precision_single <- function(error, target = 0) {
   bias <- cmean(error)
 
   return(data.frame(precision = precision, bias = bias))
+}
+
+
+
+# get mean absolute error -------------------------------------------------
+#' Obtain mean absolute
+#' @export
+get_mean_absolute_error <- function(error){
+
+  error = abs(error)
+
+  # get x and y of response vectors
+  x_r <- sum(cos(error))
+  y_r <- sum(sin(error))
+
+  # get mean absolute error vector
+  mae <- atan(y_r / x_r)
+
+}
+
+# get resultant vector length ---------------------------------------------
+#' Obtain resultant vector length
+#' @export
+get_resultant_vector_length <- function(error){
+
+  # get x and y of response vectors
+  x_r <- sum(cos(error))
+  y_r <- sum(sin(error))
+
+  # get resultant vector length
+  r <- sqrt((x_r ^ 2) + (y_r ^ 2))
+
+  # normalise resultant vector length
+  r_norm <- r / length(error)
+
+  return(r_norm)
+
 }
 
